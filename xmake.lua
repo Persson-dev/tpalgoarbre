0bb3988f22e0999364022cb226f8bc8a137fa508
Persson-dev <sim16.prib@gmail.com>
2025-01-14 16:28:18 +0100

    matplot

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,9 @@
+add_requires("matplotplusplus")
+
 target("avl_bench")
     set_kind("binary")
     add_files("avl_bench.cpp")
+    add_packages("matplotplusplus")
 
 target("avl")
     set_kind("binary")
